d2c8f835bfc9c3982a9fb9dd9af8019b0782134e
Will Smart <wjsmart12@gmail.com>
2021-10-01 19:35:30 -0500

    Add style

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 
 const schedule = {
   "title": "CS Courses for 2020-2021",
@@ -37,25 +38,30 @@ const getCourseNumber = course => (
 );
 
 const Course = ({ course }) => (
-  <div>
-    { getCourseTerm(course) } CS { getCourseNumber(course) }: { course.title }
+  <div className="card m-2 p-2">
+    <div className="card-body">
+      <div className="card-title">{ getCourseTerm(course) } CS { getCourseNumber(course) }</div>
+      <div className="card-text">{ course.title }</div>
+    </div>
   </div>
 );
+
 
 const Banner = ({ title }) => (
   <h1>{ title }</h1>
 );
 
 const CourseList = ({ courses }) => (
-  <div>
-  { Object.values(courses).map(course => <Course course={ course } />) }
+  <div className="course-list">
+  { Object.values(courses).map(course => <Course key={course.id} course={ course } />) }
   </div>
 );
 
+
 const App = () =>  (
-  <div>
-    <Banner title = {schedule.title} />
-    <CourseList courses = {schedule.courses} />
+  <div className="container">
+    <Banner title={ schedule.title } />
+    <CourseList courses={ schedule.courses } />
   </div>
 );
 
