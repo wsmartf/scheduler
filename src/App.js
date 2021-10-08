3d7943dc3df69c8f5ab2f5075485aeebb155d229
Will Smart <wjsmart12@gmail.com>
2021-10-07 21:02:47 -0500

    App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { addScheduleTimes } from './utilities/times.js';
 import CourseList from './components/CourseList.js';
 import { useData } from './utilities/firebase.js';
@@ -10,7 +10,7 @@ const Banner = ({ title }) => (
 
 const App = () => {
   const [schedule, loading, error] = useData('/', addScheduleTimes); 
-  
+
   if (error) return <h1>{error}</h1>;
   if (loading) return <h1>Loading the schedule...</h1>
 
